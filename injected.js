e112c4e6c18cba7c034af1c38ab9438dfcbe3322
KaRoL419 <85129815+KaRoL419@users.noreply.github.com>
2021-06-01 00:18:48 +0300

    Update injected.js

--- injected.js
+++ injected.js
@@ -129,8 +129,7 @@ var skinsexception = [
     "Navaja Knife | Tiger Tooth (Minimal Wear)",
     "Stiletto Knife | Boreal Forest (Field-Tested)",
     "StatTrak Gut Knife | Doppler (Factory New) Phase 4",
-    "Gut Knife | Gamma Doppler (Factory New) Phase 4",
-    "StatTrak Gut Knife | Gamma Doppler (Factory New) Phase 3"
+    "Gut Knife | Gamma Doppler (Factory New) Phase 4"
 ];
 
 
