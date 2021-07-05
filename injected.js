b695b8bbbabe1118af5a15a312e723f64ffc53e7
KaRoL419 <85129815+KaRoL419@users.noreply.github.com>
2021-07-05 18:13:39 +0300

    Update injected.js

--- injected.js
+++ injected.js
@@ -445,6 +445,7 @@ function confirmerror() {
             loginerror = true;
         }
         else $('.window.window_status.window_error.window_warning_wide.opened button').click();
+        if ($('p:contains("Error connecting to p2p server. Please reload the page")').length > 0) {reloadpage()};
     }
     if($('.active_p2p_exchange.opened').length == 1 && window.location.href == "https://csgopolygon.gg/P2PWithdraw.php") $('.active_p2p_exchange.opened .p2p_confirm').click();
 }
@@ -488,7 +489,6 @@ function togglewithdrawknifes() {
 function turnon() {
     if (!activebot) {
         if($('.active_p2p_exchange.opened').length == 1) $('.active_p2p_exchange.opened .p2p_confirm').click();
-        if ($('p:contains("Error connecting to p2p server. Please reload the page")').length > 0) {reloadpage()};
         //timerId = setInterval(unexpected, 3000);
         timerId2 = setInterval(confirmerror, 3000);
         timerId3 = setInterval(function() {
