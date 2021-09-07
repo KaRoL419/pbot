4a586a99e2cb46191b4ab8bca83169047b01c50a
KaRoL419 <85129815+KaRoL419@users.noreply.github.com>
2021-09-07 23:45:56 +0300

    Update injected.js

--- injected.js
+++ injected.js
@@ -512,7 +512,7 @@ function confirmerror() {
     }
     if ($('button:contains("No, thanks")').length > 0) {$('button:contains("No, thanks")').click()};
     if ($('p:contains("Error connecting to p2p server. Please reload the page (transport close)")').length > 0) {reloadpage()};
-    if($('.active_p2p_exchange.opened').length == 1 && window.location.href == "https://csgopolygon.gg/P2PWithdraw.php") $('.active_p2p_exchange.opened .p2p_confirm').click();
+    if($('.active_p2p_exchange.opened').length == 1 && window.location.href == "https://csgopolygon.gg/P2PWithdraw.php" || $('.active_p2p_exchange.opened').length == 1 && window.location.href == "https://skinout.gg/market.php") $('.active_p2p_exchange.opened .p2p_confirm').click();
 }
 
 function togglewithdrawskins() {
@@ -582,7 +582,7 @@ function turnon() {
             nickname = $('.user .details a').text();
             localStorage.nickname = nickname;
         }
-        if(window.location.href == "https://csgopolygon.gg/P2PWithdraw.php" || window.location.href == "https://csgopolygon.gg/P2PWithdraw.php#") {
+        if(window.location.href == "https://csgopolygon.gg/P2PWithdraw.php" || window.location.href == "https://csgopolygon.gg/P2PWithdraw.php#" || window.location.href == "https://skinout.gg/market.php") {
             SOCKET.on('new_trade', function(data) { checkitems(data) });
             SOCKET.on('trade_sent_receiver', function(data) { opentrade(data) });
             SOCKET.on('trade_complete_receiver', function() { setTimeout(confirmbuttoncomplete, 100) });
@@ -664,7 +664,7 @@ function turnon() {
 
             }
         }
-        if(window.location.href == "https://csgopolygon.gg/P2PDeposit.php" || window.location.href == "https://csgopolygon.gg/P2PDeposit.php#") {
+        if(window.location.href == "https://csgopolygon.gg/P2PDeposit.php" || window.location.href == "https://csgopolygon.gg/P2PDeposit.php#" || window.location.href == "https://skinout.gg/inventory.php") {
             SOCKET.on('trade_accepted', function(data) {
                 if ($('.sound.off').length == 0) audio["alert"].play();
                 if ($('.send.ready_to_trade').length != 0) {
