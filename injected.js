e80c83bff5adff2f63078173407e9e6a4bd42ec7
KaRoL419 <85129815+KaRoL419@users.noreply.github.com>
2022-02-21 23:07:53 +0200

    Update injected.js

--- injected.js
+++ injected.js
@@ -507,7 +507,7 @@ function unexpected() {
 }
 
 function confirmerror() {
-    if ($('.window.window_status.window_error.window_warning_wide.opened button').length > 0) {
+    if ($('.window.window_status.window_warning_wide.opened button').length > 0) {
         if ($('.opened.window_error.window_warning_wide').text() == "ErrorSession expired. Please log in and try again.OK" && loginerror == false) {
             $('.window.window_status.opened.window_notify.window_warning_wide .cancel').click()
             $.ajax({
