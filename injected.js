bdeccc9900108da4b2823abc423627cf96114755
KaRoL419 <85129815+KaRoL419@users.noreply.github.com>
2023-01-08 18:48:50 +0200

    Update injected.js

--- injected.js
+++ injected.js
@@ -748,7 +748,7 @@ function turnon() {
 	} else {$(".skins_row_item_unit:nth-child("+i+")").css("box-shadow", "inset 0px 0px 0px 5px red");
 	$(".skins_row_item_unit:nth-child("+i+") .skins_item_actions").append('<button class="skins_item_button sibred" style="height: 35px; background: cadetblue;" onclick = "chprice('+i+')"><span style="line-height: unset; padding: 0;">change price</span></button>');}
                 }
-		    if (+$(".skins_row_item_unit:nth-child("+i+") .skins_item_requests_list li:nth-child(2) span:nth-child(1)").html() +6 <= +$(".skins_row_item_unit:nth-child("+i+") .skins_item_cost input").val()) {
+		    if (+$(".skins_row_item_unit:nth-child("+i+") .skins_item_requests_list li:nth-child(2) span:nth-child(1)").html() + 6 <= +$(".skins_row_item_unit:nth-child("+i+") .skins_item_cost input").val()) {
 			    $(".skins_row_item_unit:nth-child("+i+")").css("box-shadow", "inset 0px 0px 0px 5px green");
 	$(".skins_row_item_unit:nth-child("+i+") .skins_item_actions").append('<button class="skins_item_button sibgreen" style="height: 35px; background: cadetblue;" onclick = "chprice('+i+')"><span style="line-height: unset; padding: 0;">change price</span></button>');
 		    }
